f7564590df9e3e3d0460f80164c401b657ad740e
Sam Han <shan@8thlight.com>
2015-09-07 11:19:21 +0100

    Improves EasyAI test coverage

--- src/test/java/com/samhan/player/EasyComputerTest.java
+++ src/test/java/com/samhan/player/EasyComputerTest.java
@@ -4,8 +4,11 @@ import com.samhan.Board;
 import com.samhan.Marker;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 public class EasyComputerTest {
     @Test
@@ -27,5 +30,17 @@ public class EasyComputerTest {
         int computerMove = computer.nextMove(board);
 
         assertThat(computerMove, is(8));
+    }
+
+    @Test
+    public void choosesFromOnlyAvailableMoves() {
+        Board board = new Board(3);
+        EasyComputer computer = new EasyComputer(Marker.O);
+        List<Integer> availableMoves = board.availableMoves();
+
+        int computerMove = computer.nextMove(board);
+        int anotherMove = computer.nextMove(board);
+
+        assertThat(availableMoves, hasItems(computerMove, anotherMove));
     }
 }
